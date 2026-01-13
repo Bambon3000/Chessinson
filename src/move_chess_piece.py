7b05f13d493b43fc9090ddbd05c2b2177e131e0e
Bambon3000 <120419685+Bambon3000@users.noreply.github.com>
2026-01-13 11:16:29 +0100

    Update move_chess_piece

--- src/move_chess_piece.py
+++ src/move_chess_piece.py
@@ -12,7 +12,7 @@ To get started, open a terminal and type:
 
 Then change to this directory and type:
 
-    python3 joint_position_control.py
+    python3 move_chess_piece.py
 """
 
 
@@ -41,8 +41,8 @@ class Chess_Robot:
 		
 		
 	def robot_move(self, from_x, to_x, from_y, to_y):
-		above_z = 0.3
-		on_z    = 0.25
+		above_z = 0.35
+		on_z    = 0.3
 		
 		bot = self.bot
 		
@@ -62,7 +62,41 @@ class Chess_Robot:
 		bot.arm.set_ee_pose_components(x=to_x, y=to_y, z=above_z, roll=0.0, pitch=0.0, yaw=0.0)
 		
 		bot.arm.go_to_home_pose()
+
+	def robot_take(self, from_x, to_x, from_y, to_y):
+		above_z = 0.35
+		on_z    = 0.3
+
+		trash_x   = 0.25
+		trash_y   = -0.13
+		trash_yaw = -0.75
 		
+		bot = self.bot
+		
+		bot.arm.go_to_home_pose()
+		bot.gripper.release()
+
+		# grab enemy piece
+		bot.arm.set_ee_pose_components(x=to_x, y=to_y, z=above_z, roll=0.0, pitch=0.0, yaw=0.0)
+		bot.arm.set_ee_pose_components(x=to_x, y=to_y, z=on_z, roll=0.0, pitch=0.0, yaw=0.0)
+		bot.gripper.grasp()
+		bot.arm.set_ee_pose_components(x=to_x, y=to_y, z=above_z, roll=0.0, pitch=0.0, yaw=0.0)
+		bot.arm.set_ee_pose_components(x=trash_x, y=trash_y, z=above_z, roll=0.0, pitch=0.0, yaw=-0.75)
+		bot.gripper.release()
+
+		# grab piece
+		bot.arm.set_ee_pose_components(x=from_x, y=from_y, z=above_z, roll=0.0, pitch=0.0, yaw=0.0)
+		bot.arm.set_ee_pose_components(x=from_x, y=from_y, z=on_z, roll=0.0, pitch=0.0, yaw=0.0)
+		bot.gripper.grasp()
+		bot.arm.set_ee_pose_components(x=from_x, y=from_y, z=above_z, roll=0.0, pitch=0.0, yaw=0.0)
+		
+		# move piece to target position
+		bot.arm.set_ee_pose_components(x=to_x, y=to_y, z=above_z, roll=0.0, pitch=0.0, yaw=0.0)
+		bot.arm.set_ee_pose_components(x=to_x, y=to_y, z=on_z, roll=0.0, pitch=0.0, yaw=0.0)
+		bot.gripper.release()
+		bot.arm.set_ee_pose_components(x=to_x, y=to_y, z=above_z, roll=0.0, pitch=0.0, yaw=0.0)
+		
+		bot.arm.go_to_home_pose()
 
 	if __name__ == '__robot_move__':
 		robot_move()
